d63e1156b1b883c378e62f54239b4a322923d8af
Bram <bgerritsen@emico.nl>
2023-12-01 18:22:09 +0100

    fix syntax error

--- day1/main.php
+++ day1/main.php
@@ -4,10 +4,10 @@ $lines = explode("\n", file_get_contents(__DIR__ . '/input.txt'));
 $map = ['one' => 1, 'two' => 2, 'three' => 3, 'four' => 4, 'five' => 5, 'six' => 6, 'seven' => 7, 'eight' => 8, 'nine' => 9,];
 $sum = 0;
 foreach ($lines as $line) {
-    $pattern = '/(?=(' . implode('|', array_keys($map)). '|\d{1}))/';
+    $pattern = '/(?=(' . implode('|', array_keys($map)). '|\d))/';
     preg_match_all($pattern, $line, $matches);
 
-    $firstNum = $map($matches[1][0]) ?? $matches[1][0];
+    $firstNum = $map[$matches[1][0]] ?? $matches[1][0];
     $secondNum = $matches[1][count($matches[1])-1];
     if (isset($map[$secondNum])) {
         $secondNum = $map[$secondNum];
