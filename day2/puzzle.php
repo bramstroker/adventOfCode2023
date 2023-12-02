cedfe01e2f79c6a2a9483fd695cd7826c9107ab3
Bram <bgerritsen@emico.nl>
2023-12-02 09:09:01 +0100

    Solve day 2 - part 2

--- day2/puzzle.php
+++ day2/puzzle.php
@@ -1,30 +1,56 @@
 <?php
 $lines = explode("\n", file_get_contents(__DIR__ . '/input.txt'));
 
-$bag = new Bag([
-    new CubeCount('red', 12),
-    new CubeCount('green', 13),
-    new CubeCount('blue', 14),
-]);
+function solve1(array $lines): int
+{
+    $createBag = function() {
+        return new Bag([
+            new CubeCount('red', 12),
+            new CubeCount('green', 13),
+            new CubeCount('blue', 14),
+        ]);
+    };
 
-$validGames = [];
-foreach ($lines as $line) {
-    $game = Game::parse($line);
-    foreach ($game->draws as $draw) {
-        $validGames[$game->id] = $game->id;
-        $freshBag = clone $bag;
-        try {
-            foreach ($draw->cubeCounts as $cubeCount) {
-                $freshBag->draw($cubeCount);
+    $validGames = [];
+    foreach ($lines as $line) {
+        $game = Game::parse($line);
+        foreach ($game->draws as $draw) {
+            $validGames[$game->id] = $game->id;
+            $bag = $createBag();
+            try {
+                foreach ($draw->cubeCounts as $cubeCount) {
+                    $bag->draw($cubeCount);
+                }
+            } catch (Exception $e) {
+                unset($validGames[$game->id]);
+                continue 2;
             }
-        } catch (Exception $e) {
-            unset($validGames[$game->id]);
-            continue 2;
         }
     }
+    return array_reduce($validGames, fn(int $carry, int $gameNumber) => $carry + $gameNumber, 0);
 }
-$idSum = array_reduce($validGames, fn(int $carry, int $gameNumber) => $carry + $gameNumber, 0);
-echo 'Solution = ' . $idSum;
+
+function solve2(array $lines): int
+{
+    $sum = 0;
+    foreach ($lines as $line) {
+        $game = Game::parse($line);
+
+        $highestCountPerColor = [];
+        foreach ($game->draws as $draw) {
+            foreach ($draw->cubeCounts as $cubeCount) {
+                if (!isset($highestCountPerColor[$cubeCount->color]) || $highestCountPerColor[$cubeCount->color] < $cubeCount->amount) {
+                    $highestCountPerColor[$cubeCount->color] = $cubeCount->amount;
+                }
+            }
+        }
+        $sum += array_product($highestCountPerColor);
+    }
+    return $sum;
+}
+
+echo 'Solution 1 = ' . solve1($lines) . PHP_EOL;
+echo 'Solution 2 = ' . solve2($lines) . PHP_EOL;
 
 class CubeCount
 {
@@ -72,6 +98,10 @@ class Draw
 
 class Game
 {
+    /**
+     * @param int    $id
+     * @param Draw[] $draws
+     */
     public function __construct(public int $id, public array $draws)
     {
     }
