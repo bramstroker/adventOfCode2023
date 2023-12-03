087fe6c2259bc0a1a6bc38a58bf6db0b25539e70
Bram <bgerritsen@emico.nl>
2023-12-03 07:08:01 +0100

    Remove duplicate tests

--- day3/puzzle.php
+++ day3/puzzle.php
@@ -1,111 +1,51 @@
 <?php
 $lines = explode("\n", file_get_contents(__DIR__ . '/input.txt'));
 
-function solve1(array $lines): int
-{
-    $createBag = function() {
-        return new Bag([
-            new CubeCount('red', 12),
-            new CubeCount('green', 13),
-            new CubeCount('blue', 14),
-        ]);
-    };
+$grid = array_map(fn($line) => str_split($line), $lines);
 
-    $validGames = [];
-    foreach ($lines as $line) {
-        $game = Game::parse($line);
-        foreach ($game->draws as $draw) {
-            $validGames[$game->id] = $game->id;
-            $bag = $createBag();
-            try {
-                foreach ($draw->cubeCounts as $cubeCount) {
-                    $bag->draw($cubeCount);
-                }
-            } catch (Exception $e) {
-                unset($validGames[$game->id]);
-                continue 2;
-            }
-        }
-    }
-    return array_reduce($validGames, fn(int $carry, int $gameNumber) => $carry + $gameNumber, 0);
-}
-
-function solve2(array $lines): int
+function solve1(array $grid): int
 {
     $sum = 0;
-    foreach ($lines as $line) {
-        $game = Game::parse($line);
+    foreach ($grid as $y => $line) {
+        preg_match_all('/\d+/', implode("", $line), $matches, PREG_OFFSET_CAPTURE);
 
-        $highestCountPerColor = [];
-        foreach ($game->draws as $draw) {
-            foreach ($draw->cubeCounts as $cubeCount) {
-                if (($highestCountPerColor[$cubeCount->color] ?? 0) < $cubeCount->amount) {
-                    $highestCountPerColor[$cubeCount->color] = $cubeCount->amount;
+        foreach ($matches[0] as $match) {
+            $number = $match[0];
+            $offset = $match[1];
+            $endOffset = $offset + strlen($number);
+            for ($x = $offset; $x < $endOffset; $x++) {
+                $testRight = $x === ($endOffset - 1);
+                $testLeft = $x === $offset;
+                if (is_surrounded_by_symbol($grid, $x, $y, $testLeft, $testRight)) {
+                    echo 'Symbol found adjacent to ' . $number . ' at ' . $x . ',' . $y . PHP_EOL;
+                    $sum += $number;
+                    continue 2;
                 }
             }
         }
-        $sum += array_product($highestCountPerColor);
     }
     return $sum;
 }
 
-echo 'Solution 1 = ' . solve1($lines) . PHP_EOL;
-echo 'Solution 2 = ' . solve2($lines) . PHP_EOL;
-
-class CubeCount
-{
-    public function __construct(public string $color, public int $amount)
-    {
-    }
-
-    public static function parse(string $string): CubeCount
-    {
-        $parts = explode(' ', $string);
-        return new CubeCount(trim($parts[1]), (int) trim($parts[0]));
-    }
+function is_surrounded_by_symbol(
+    array $grid,
+    int $x,
+    int $y,
+    bool $testLeft = true,
+    bool $testRight = true
+): bool {
+    $right = $testRight ? ($grid[$y][$x+1] ?? '.') : '.';
+    $left = $testLeft ? ($grid[$y][$x-1] ?? '.') : '.';
+    return (
+        $left !== '.' ||
+        $right !== '.' ||
+        ($grid[$y-1][$x] ?? '.') !== '.' ||
+        ($grid[$y-1][$x-1] ?? '.') !== '.' ||
+        ($grid[$y-1][$x+1] ?? '.') !== '.' ||
+        ($grid[$y+1][$x] ?? '.') !== '.' ||
+        ($grid[$y+1][$x-1] ?? '.') !== '.' ||
+        ($grid[$y+1][$x+1] ?? '.') !== '.'
+    );
 }
 
-class Bag
-{
-    public function __construct(public array $cubeCounts)
-    {
-    }
-
-    public function draw(CubeCount $cubeCount): void
-    {
-        foreach ($this->cubeCounts as $bagCount) {
-            if ($bagCount->color === $cubeCount->color) {
-                $bagCount->amount -= $cubeCount->amount;
-                if ($bagCount->amount < 0) {
-                    throw new \Exception('Not enough cubes in the bag');
-                }
-            }
-        }
-    }
-}
-
-class Draw
-{
-    public function __construct(public array $cubeCounts)
-    {
-    }
-}
-
-class Game
-{
-    public function __construct(public int $id, public array $draws)
-    {
-    }
-
-    public static function parse($string): Game
-    {
-        $draws = [];
-        preg_match('/Game ([0-9]+): (.*)/', $string, $matches);
-        foreach (explode(';', $matches[2]) as $parsedDraw) {
-            $draws[] = new Draw(
-                array_map(fn(string $cubeCount) => CubeCount::parse($cubeCount), explode(', ', trim($parsedDraw)))
-            );
-        }
-        return new Game($matches[1], $draws);
-    }
-}
+echo 'Solution 1 = ' . solve1($grid) . PHP_EOL;
