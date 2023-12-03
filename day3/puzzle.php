19b4745bcd7c2a093383b444668c0c6ecc0bf34b
Bram <bgerritsen@emico.nl>
2023-12-03 09:09:01 +0100

    Commit WIP

--- day3/puzzle.php
+++ day3/puzzle.php
@@ -22,7 +22,7 @@ function solve1(array $grid): int
                     $directions[] = Direction::Left;
                 }
                 $test = fn(string $char) => $char !== '.';
-                if (find_adjacent($grid, $x, $y, $test, $directions) !== null) {
+                if (!empty(findAdjacent($grid, $x, $y, $test, $directions))) {
                     echo 'Symbol found adjacent to ' . $number . ' at ' . $x . ',' . $y . PHP_EOL;
                     $sum += $number;
                     continue 2;
@@ -36,44 +36,86 @@ function solve1(array $grid): int
 function solve2(array $grid): int
 {
     $sum = 0;
+    $totalGearsFound = 0;
     foreach ($grid as $y => $line) {
         foreach ($line as $x => $char) {
             if ($char !== '*') {
                 continue;
             }
 
-            $numbersAt = array_values(array_filter([
-                find_adjacent($grid, $x, $y, 'is_numeric', [Direction::Top]),
-                find_adjacent($grid, $x, $y, 'is_numeric', [Direction::Bottom]),
-                find_adjacent($grid, $x, $y, 'is_numeric', [Direction::Left]),
-                find_adjacent($grid, $x, $y, 'is_numeric', [Direction::Right]),
-            ]));
+            # Scan adjacent bounding box for numbers
+            $numbersAt = [
+                ...findNumberCoordsAtLine($grid, $y-1, $x-1, $x+1),
+                ...findNumberCoordsAtLine($grid, $y, $x-1, $x+1),
+                ...findNumberCoordsAtLine($grid, $y+1, $x-1, $x+1),
+            ];
+
+            $surroundingGrid = printSurroundingGrid($grid, new Coordinate($x, $y));
+            echo $surroundingGrid;
+
             if (count($numbersAt) !== 2) {
                 echo 'No Gear found at ' . $y . ',' . $x . ', numberCount = ' . count($numbersAt) . PHP_EOL;
                 continue;
             }
 
-            $number1 = resolveNumber($grid, $numbersAt[0]);
-            $number2 = resolveNumber($grid, $numbersAt[1]);
+            $number1 = resoleFullNumber($grid, $numbersAt[0]);
+            $number2 = resoleFullNumber($grid, $numbersAt[1]);
             $gearRatio = $number1 * $number2;
 
+            echo PHP_EOL;
             echo 'Gear found at ' . $y . ',' . $x . PHP_EOL;
+
+
+            if (!str_contains($surroundingGrid, $number1) || !str_contains($surroundingGrid, $number2)) {
+                throw new Exception('Gear numbers not found in grid');
+            }
+
             echo 'Gear numbers = ' . $number1 . ',' . $number2 . PHP_EOL;
             echo 'Gear ratio = ' . $gearRatio . PHP_EOL;
 
             $sum += $gearRatio;
+            $totalGearsFound++;
         }
     }
+
+    echo 'Total gear = ' . $totalGearsFound;
     return $sum;
 }
 
-function find_adjacent(
+function findNumberCoordsAtLine(array $grid, int $y, int $minX, int $maxX): array
+{
+    $line = '';
+    for ($x = $minX; $x <= $maxX; $x++) {
+        $line .= $grid[$y][$x];
+    }
+    preg_match_all('/\d+/', $line, $matches, PREG_OFFSET_CAPTURE);
+    if (!count($matches[0])) {
+        return [];
+    }
+
+    return array_map(fn($match) => new Coordinate($match[1] + $minX, $y), $matches[0]);
+}
+
+function printSurroundingGrid(array $grid, Coordinate $coordinate)
+{
+    $output = '';
+    for ($y = $coordinate->y - 1; $y <= $coordinate->y + 2; $y++) {
+        $line = '';
+        for ($x = $coordinate->x - 3; $x <= $coordinate->x + 3; $x++) {
+            $line .= $grid[$y][$x] ?? ' ';
+        }
+        $output .= $line . PHP_EOL;
+    }
+    return $output;
+}
+
+function findAdjacent(
     array $grid,
     int $x,
     int $y,
     callable $test,
     array $directions = [Direction::Top, Direction::Left, Direction::Right, Direction::Bottom]
-): ?Coordinate {
+): array {
     $coordsToTest = [
         in_array(Direction::Right, $directions) ? [$y, $x+1] : null,
         in_array(Direction::Left, $directions) ? [$y, $x-1] : null,
@@ -85,16 +127,17 @@ function find_adjacent(
         in_array(Direction::Bottom, $directions) ? [$y+1, $x+1] : null,
     ];
 
+    $found = [];
     foreach (array_filter($coordsToTest) as $yx) {
         $value = $grid[$yx[0]][$yx[1]] ?? null;
         if ($value !== null && $test($value)) {
-            return new Coordinate($yx[1], $yx[0]);
+            $found[] = new Coordinate($yx[1], $yx[0]);
         }
     }
-    return null;
+    return $found;
 }
 
-function resolveNumber(array $grid, Coordinate $coord): int
+function resoleFullNumber(array $grid, Coordinate $coord): int
 {
     $x = $coord->x;
     $y = $coord->y;
@@ -131,5 +174,5 @@ class Coordinate {
     ) {}
 }
 
-echo 'Solution 1 = ' . solve1($grid) . PHP_EOL;
+//echo 'Solution 1 = ' . solve1($grid) . PHP_EOL;
 echo 'Solution 1 = ' . solve2($grid) . PHP_EOL;
